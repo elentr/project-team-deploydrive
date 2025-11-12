73018995c6dd1dd0d88009de6c9868d77acd5de3
Roksolana <adelaw283@gmail.com>
2025-11-12 21:23:08 +0200

    stories

---
+++ types/stories.ts
@@ -0,0 +1,12 @@
+export interface Story {
+  _id: string;
+  storyImage: string;
+  title: string;
+  article: string;
+  category: string;
+  ownerId: string;
+  date: string;
+  favoriteCount: number;
+  createdAt: string;
+  updatedAt: string;
+}
