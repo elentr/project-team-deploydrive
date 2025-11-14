019818ff9dadfd348705c4239f8f09c0b96739f8
Anastasia1102 <doitinyourownway.f@gmail.com>
2025-11-14 22:46:22 +0000

    база AddStoryPage
    прописаний css та частина основної логіки форми

---
+++ app/(private routes)/stories/create/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import CreateStory from "@/components/CreateStory/CreateStory";
+
+
+export default function CreateStoryPage() {
+return <CreateStory />;
+}
--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { clientApi } from "./clientApi";
+import type { Category, CreateStoryResponse } from "@/types/story";
 
 const baseURL = process.env.NEXT_PUBLIC_API_URL + '/api';
 
@@ -9,3 +11,14 @@ export const apiClient = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+export async function createStory(formData: FormData): Promise<CreateStoryResponse> {
+  const { data } = await clientApi.post<CreateStoryResponse>("/api/stories", formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+  return data;
+}
+export async function fetchCategories(): Promise<Category[]> {
+  const { data } = await clientApi.get("/api/categories");
+  return data as Category[];
+}
