0c1f9a66ef1ce1d75785cab60cc25a1d3fb43696
Serge <55316965+Gorodeikin@users.noreply.github.com>
2025-11-17 00:07:40 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,20 @@
+import styles from './page.module.css';
+
+import Hero from '@/components/Hero/Hero';
+import Popular from '@/components/Popular/Popular';
+import OurTravellers from '@/components/OurTravellers/OurTravellers';
+import About from '@/components/About/About';
+import Join from '@/components/Join/Join';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Popular />
+      <OurTravellers />
+      <Join />
+    </>
+  );
+}
 
