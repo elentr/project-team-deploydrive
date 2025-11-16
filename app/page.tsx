1676e3dfd72fd298a1ff590f00671adeb8ce93d3
Serge <55316965+Gorodeikin@users.noreply.github.com>
2025-11-17 00:16:59 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,28 @@
+import styles from "./page.module.css";
+import Link from "next/link";
 
+
+import Hero from "@/components/Hero/Hero";
+// import Popular from "@/components/Popular/Popular";
+// import TravellersList from "@/components/TravellersList/TravellersList";
+import About from "@/components/About/About";
+import Join from "@/components/Join/Join";
+
+export default function Home() {
+  return (
+    <>
+      <Hero/>
+      <About />
+      {/* <Popular /> */}
+      {/* <TravellersList /> */}
+      <Join />
+    </>
+  );
+}
+
+<Link
+  href="/stories"
+  className="text-blue-600 underline hover:text-blue-800"
+>
+  Переглянути всі історії
+</Link>
